9f23cbc0c794cd63fa0b40a6cf2f7ae9da100d01
Vladimir Poboykin <vovar3573@mail.ru>
2022-12-17 01:14:13 +0300

    Task6 has been solved

--- Homework_sem1_15122022/Task8/Program.cs
+++ Homework_sem1_15122022/Task8/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Write("Введите натуральное число: ");
+int a = int.Parse(ReadLine()!);
+int half_a = (a - a % 2) / 2; //решил не упускать возможности сократить количество итераций вдвое 
+string st = "";
+if (a == 1) {st = "отсутствуют";}
+int i = 1;
+WriteLine($"Четные числа от 1 до {a}: "); //1 - не является четным, но формулирую, как в условии задачи
+while (i <= half_a) {
+    Write($"{i*2} ");
+    i++;
+    }
+Write(st);   
