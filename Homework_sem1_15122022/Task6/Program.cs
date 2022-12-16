eee9129375fc386f255d92a7437b4e6b2c4979e0
Vladimir Poboykin <vovar3573@mail.ru>
2022-12-17 00:48:26 +0300

    Task6 has been solved

--- Homework_sem1_15122022/Task6/Program.cs
+++ Homework_sem1_15122022/Task6/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Write("Введите число: ");
+int a = int.Parse(ReadLine()!);
+if (a % 2 == 0) {
+    WriteLine($"Число {a} является четным.");
+    }
+else {
+    WriteLine($"Число {a} является НЕчетным.");
+    }    
