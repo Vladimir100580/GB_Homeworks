21a5477bbea8ef75e54c05253c4300069fc9501e
Vladimir Poboykin <vovar3573@mail.ru>
2022-12-17 00:41:05 +0300

    Task4 has been solved

--- Homework_sem1_15122022/Task4/Program.cs
+++ Homework_sem1_15122022/Task4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Write("Введите первое число: ");
+int a = int.Parse(ReadLine()!);
+Write("Введите второе число: ");
+int b = int.Parse(ReadLine()!);
+Write("Введите третье число: ");
+int c = int.Parse(ReadLine()!);
+int m = a;
+if (b > m) {
+    m = b;
+    }
+if (c > m) {
+    m = c;
+    }
+WriteLine($"Максимальное из введенных чисел: {m}");
