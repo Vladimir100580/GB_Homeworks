f928464ef658013dc6bb72e5a460dca4a86d9762
Vladimir Poboykin <vovar3573@mail.ru>
2022-12-19 22:19:41 +0300

    Task10_is_solved

--- Homework_sem2_19122022/Task10/Program.cs
+++ Homework_sem2_19122022/Task10/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Write("Введите трехзначное число: ");
+int a = int.Parse(ReadLine()!);
+WriteLine(((a-a%10)/10)%10); //((a-a%10)/10) - целочисленно делим a на 10
