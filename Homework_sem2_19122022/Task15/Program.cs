588e65791b5092ddb70c5cb4e09ef87f94d7070c
Vladimir Poboykin <vovar3573@mail.ru>
2022-12-19 23:12:09 +0300

    Task15_is_solved

--- Homework_sem2_19122022/Task15/Program.cs
+++ Homework_sem2_19122022/Task15/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Write("Введите номер дне недели: ");
+int a = int.Parse(ReadLine()!);
+WriteLine(a>0&&a<8?a>5?"Да":"Нет":"Число должно быть целым от 1 до 7");
