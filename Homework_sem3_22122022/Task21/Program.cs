d3144cd34f887a34255e2004e717130f47806345
Vladimir Poboykin <vovar3573@mail.ru>
2022-12-23 00:01:21 +0300

    Task21 is solved

--- Homework_sem3_22122022/Task21/Program.cs
+++ Homework_sem3_22122022/Task21/Program.cs
@@ -1,13 +1,19 @@
 ﻿using static System.Console;
 
-WriteLine("Введите первую координату x: ");
+WriteLine("Введите абсциссу первой точки x1: ");
 int x1 = int.Parse(ReadLine()!);
-WriteLine("Введите первую координату y: ");
+WriteLine("Введите ординату первой точки y1: ");
 int y1 = int.Parse(ReadLine()!);
-WriteLine("Введите вторую координату x: ");
-int x2 = int.Parse(ReadLine()!);
-WriteLine("Введите вторую координату y: ");
-int y2 = int.Parse(ReadLine()!);
-double l = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+WriteLine("Введите аппликату первой точки z1: ");
+int z1 = int.Parse(ReadLine()!);
 
-WriteLine($"Длинна отрезка между точками равна {l}");
+WriteLine("Введите абсциссу второй точки x2: ");
+int x2 = int.Parse(ReadLine()!);
+WriteLine("Введите ординату второй точки y2: ");
+int y2 = int.Parse(ReadLine()!);
+WriteLine("Введите аппликату второй точки z2: ");
+int z2 = int.Parse(ReadLine()!);
+
+double l = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1));
+
+WriteLine($"Расстояние между точками равно {l:f2}");
