6dd3f3dbe01c62518dd7e1820b3b995a3b56b3d9
Vladimir Poboykin <vovar3573@mail.ru>
2023-01-12 22:40:36 +0300

    Task 25 is completed

--- Homework_sem4_12012023/Task25/Program.cs
+++ Homework_sem4_12012023/Task25/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Напишите программу, реализующую метод,который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B с использованием цикла.
+
+using static System.Console;
+
+WriteLine("Введите основание А: ");
+double A = double.Parse(ReadLine()!);      // в условии не указана "натуральность" числа A, поэтому double
+WriteLine("Введите показатель степени B: ");
+int B = int.Parse(ReadLine()!);
+
+double p1 = PowAB(A, B);
+WriteLine($"A^B = {p1}");
+
+
+
+
+
+
+double PowAB(double A, int B)
+{
+    double p = 1;
+    for (int i=0; i<B; i++)
+    {
+        p *= A;
+    }
+    return p;
+}
