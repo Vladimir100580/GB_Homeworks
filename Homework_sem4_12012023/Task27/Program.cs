c513a2e8fb3d39482d16132c889c5edb5e2cf906
Vladimir Poboykin <vovar3573@mail.ru>
2023-01-12 23:59:00 +0300

    Task27 is solved

--- Homework_sem4_12012023/Task27/Program.cs
+++ Homework_sem4_12012023/Task27/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, реализующую метод, который принимает на вход число и выдаёт сумму цифр в числе.
+
+using static System.Console;
+
+WriteLine("Введите целое число: ");
+int A = int.Parse(ReadLine()!);      // подразумеваем, что вводится целое число
+
+int sum = SumDigits(A);
+WriteLine($"Сумма цифр в числе {A} равна {sum}");
+
+
+int SumDigits(int A)
+{
+    if (A < 0) A *= -1;
+    int s = 0;
+    while (A > 0)
+    {
+        s += A%10;
+        A /= 10;
+    }
+    return s;
+}
