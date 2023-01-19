e89141f63da1c5c62cf1080ca808ffece4f7f53b
Vladimir Poboykin <vovar3573@mail.ru>
2023-01-19 23:18:12 +0300

    Task41 is completed

---
+++ Homework_sem6_19012023/Task41/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+Clear();
+Write("Введите числа через пробел: ");
+string[] array = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+int n = Quantity(array);
+string numtxt = "положительных чисел";         // Создаем "красоту" склонений
+if (n%10 == 1 && (n/10)%10 != 1) numtxt = "положительное число";
+if (n%10 >= 2 && n%10 <= 4 && (n/10)%10 != 1) numtxt = "положительных числа";
+
+WriteLine($"Вы ввели {Quantity(array)} {numtxt}.");
+
+
+int Quantity(string[] str)
+{
+    int s = 0;
+    for (int i = 0; i < str.Length; i++)
+    {   
+        if (double.Parse(str[i]) > 0) s++;
+    }
+    return s;
+}
