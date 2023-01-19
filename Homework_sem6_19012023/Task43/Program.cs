f1e151bd4d26c7bf44d6ef8a0ad09a3d1889951f
Vladimir Poboykin <vovar3573@mail.ru>
2023-01-19 23:49:10 +0300

    task43 is completed

--- Homework_sem6_19012023/Task43/Program.cs
+++ Homework_sem6_19012023/Task43/Program.cs
@@ -1,3 +1,22 @@
 ﻿using static System.Console;
+Clear();
+Write("Введите через пробел b1, k1, b2, k2: ");
+string[] array = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+WriteLine($"{Intersection(array)}");
 
 
+
+
+string Intersection(string[] str)            // Эксперимент с переводом из строки и обратно
+{
+    double b1 = double.Parse(str[0]);
+    double k1 = double.Parse(str[1]);
+    double b2 = double.Parse(str[2]);
+    double k2 = double.Parse(str[3]);
+    if (k1 == k2 ) return "Прямые параллельны.";
+
+    string x = ((b2 - b1) / (k1 - k2)).ToString();
+    string y = (k1 * (b2 - b1) / (k1 - k2) + b1).ToString();
+    return "Точка пересечения прямых: (" + x + "; " + y + ")";
+}
