daa6a27a8fc5f1b3ead760f5cdbdc01709a22143
Vladimir Poboykin <vovar3573@mail.ru>
2023-01-25 00:01:48 +0300

    task47 is completed

---
+++ Homework_sem7_23012023/Task47/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу реализующую метод, 
+// который возвращает массив размером m×n, заполненный случайными вещественными числами.
+
+using static System.Console;
+Clear();
+Write("Введите 5 чисел: размерность матрицы (m, n), диапозон значений (min, max) и точность чисел (количество знаков после запятой) через пробел: ");
+
+double[] doubParams=Array.ConvertAll(ReadLine()!.Split(" ",StringSplitOptions.RemoveEmptyEntries), double.Parse);
+
+double[,] array = GetMatrixArray(doubParams[0], doubParams[1], doubParams[2], doubParams[3], doubParams[4]);
+
+PrintMatrixArray(array);
+
+
+
+
+double[,] GetMatrixArray(double rows, double columns, double minValue, double maxValue, double accuracy)
+{
+    Random ran = new Random();
+    double[,] resultArray = new double[Convert.ToInt32(rows), Convert.ToInt32(columns)];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            resultArray[i,j] = Math.Round(minValue + (maxValue - minValue) * ran.NextDouble(), Convert.ToInt32(accuracy));
+        }
+    }
+    return resultArray;
+}
+
+
+void PrintMatrixArray(double[,] inArray)
+{   
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j], 10} ");  
+        }
+        WriteLine();
+    }
+}
